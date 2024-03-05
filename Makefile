b11764a8a3520d9a5a9d098557ffcc21151e1a57
Noor <nalfayad18@gmail.com>
2024-03-05 22:03:29 +0300

    [feat] add static serving and swaggo

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 MIGRATION_DIR = ./migrations/
 DOCKER_COMPOSE_FILE = ./deployments/docker-compose.yml
-
+MAIN_DIR = ./cmd/tamra
+HANDLERS_DIR = ./internal/app/tamra/handlers
 # Build and run Docker Compose
 docker-up:
 	@echo "Building and starting Docker containers..."
@@ -18,4 +19,8 @@ migrate-down:
 	@echo "Running migrations..."
 	migrate -path $(MIGRATION_DIR) -database "postgresql://postgres:mysecretpassword@localhost:5432/tamra-postgis?sslmode=disable" down
 
-.PHONY: migrate-up migrate-down docker-up docker-down
+swagger:
+	@echo "Generating Swagger documentation..."
+	rm -rf public/docs && swag init -d $(MAIN_DIR),$(HANDLERS_DIR) -g main.go --parseInternal --parseDependency -o public/docs
+
+.PHONY: migrate-up migrate-down docker-up docker-down swagger
--- cmd/tamra/main.go
+++ cmd/tamra/main.go
@@ -3,16 +3,23 @@ package main
 import (
 	"Tamra/internal/app/tamra/handlers"
 	"Tamra/internal/app/tamra/repositories"
+	"Tamra/internal/app/tamra/router"
 	"Tamra/internal/app/tamra/services"
 	"Tamra/internal/pkg/utils"
 	"fmt"
 	"net/http"
 	"strconv"
-
-	"github.com/go-chi/chi/v5"
 	// "github.com/go-chi/chi/v5/middleware"
 )
 
+// @title Tamra API
+// @version 1
+// @description This is the API for the Tamra application
+// @host localhost:8080
+// @BasePath /api/v1
+// @schemes http
+// @produce json
+// @consumes json
 func main() {
 	// Read the configuration
 	config := utils.GetConfig()
@@ -43,33 +50,10 @@ func main() {
 	restaurantHandler := handlers.NewRestaurantHandler(restaurantService, validator, logger)
 	orderHandler := handlers.NewOrderHandler(orderService, validator, logger)
 
-	// Use chi as the router
-	r := chi.NewRouter()
+	r := router.NewRouter(userHandler, restaurantHandler, orderHandler)
 
-	// Define the routes
-	r.Route("/users", func(r chi.Router) {
-		r.Post("/", userHandler.CreateUser)
-		r.Get("/", userHandler.GetUsers)
-		r.Get("/{id}", userHandler.GetUser)
-		r.Patch("/{id}", userHandler.UpdateUser)
-		r.Delete("/{id}", userHandler.DeleteUser)
-	})
-
-	r.Route("/restaurants", func(r chi.Router) {
-		r.Post("/", restaurantHandler.CreateRestaurant)
-		r.Get("/", restaurantHandler.GetRestaurants)
-		r.Get("/{id}", restaurantHandler.GetRestaurant)
-		r.Patch("/{id}", restaurantHandler.UpdateRestaurant)
-		r.Delete("/{id}", restaurantHandler.DeleteRestaurant)
-	})
-
-	r.Route("/orders", func(r chi.Router) {
-		r.Post("/", orderHandler.CreateOrder)
-		r.Get("/", orderHandler.GetOrders)
-		r.Get("/{id}", orderHandler.GetOrder)
-		r.Patch("/{id}", orderHandler.UpdateOrder)
-		r.Delete("/{id}", orderHandler.DeleteOrder)
-	})
+	// TODO: Why can we call the api without including /api/v1 in the URL?
+	r.Mount("/api/v1", r)
 
 	// Start the server with the port from the configuration and cast the port to a string
 	strPort := ":" + strconv.Itoa(config.Port)
