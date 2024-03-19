64132fc03e6150411b77759efaa9e53963a7bd2e
Noor <nalfayad18@gmail.com>
2024-03-19 07:16:24 +0300

    remove conditional

--- internal/app/tamra/repositories/repo_test.go
+++ internal/app/tamra/repositories/repo_test.go
@@ -11,14 +11,7 @@ var Db *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	var dbConnectionString string
-	// Check if the code is being run locally
-	if os.Getenv("PIPELINE_BRANCH") != "" {
-		// If it is running inside of codebuild use the db as host name since it's the name of the service in the docker-compose file
-		dbConnectionString = "postgresql://postgres:mysecretpassword@db:5432/tamra-postgis-test?sslmode=disable"
-	} else {
-		dbConnectionString = "postgresql://postgres:mysecretpassword@localhost:5432/tamra-postgis-test?sslmode=disable"
-	}
+	dbConnectionString := "postgresql://postgres:mysecretpassword@db:5432/tamra-postgis-test?sslmode=disable"
 
 	Db, err = utils.NewDB(dbConnectionString)
 
